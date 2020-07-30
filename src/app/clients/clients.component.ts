24efb23f0d8ae0bc0ae0486332da0991820cc0a6
Ricardo Cardona <ricardocardona.rc@gmail.com>
2020-07-30 14:42:12 -0500

    feat/clients-module Added clients module added clients service Added clients json example

---
+++ src/app/clients/clients.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Client } from './client';
+import { ClientService } from './client.service';
+
+@Component({
+  selector: 'app-clients',
+  templateUrl: './clients.component.html',
+})
+export class ClientsComponent implements OnInit {
+
+  clients: Client[];
+
+  constructor(private clientService: ClientService) { }
+
+  ngOnInit(): void {
+    this.clients = this.clientService.getClients();
+  }
+}
