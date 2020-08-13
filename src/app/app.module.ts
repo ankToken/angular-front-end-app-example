6f56fc547a2369a8537cd3ed89e0bccb4b92e767
Ricardo Cardona <ricardocardona.rc@gmail.com>
2020-08-13 10:49:53 -0500

    feat/add-forms Added CRUD form for clients

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,13 @@ import { FooterComponent } from './footer/footer.component';
 import { ClientsComponent } from './clients/clients.component';
 import { ClientService } from './clients/client.service';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './clients/form.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {path: '', redirectTo: 'clients', pathMatch: 'full'},
   {path: 'clients', component: ClientsComponent},
+  {path: 'clients/form', component: FormComponent},
 ];
 
 @NgModule({
@@ -19,11 +22,13 @@ const routes: Routes = [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ClientsComponent
+    ClientsComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [
